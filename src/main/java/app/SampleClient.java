9536f37ad5f2f52733f1e09526f11e953e6773d9
michelle <michelle.graham@dkit.ie>
2024-01-23 14:51:10 +0000

    Commented client

--- src/main/java/app/SampleClient.java
+++ src/main/java/app/SampleClient.java
@@ -5,21 +5,24 @@ import java.net.*;
 
 public class SampleClient {
     public static void main(String[] args) {
-        // Need address information
-        // PORT
-        // IP Address/hostname
         try {
-            // OUR address information
+            // SET-UP STAGE
+            // OUR address information - we listen for messages here
             int myPort = 5656;
+            // Create a socket on which to listen for messages to that port
             DatagramSocket mySocket = new DatagramSocket(myPort);
+            // Set timeout on the socket so it doesn't wait for a message for longer than 5000 milliseconds
             mySocket.setSoTimeout(5000);
 
-            // Destination address information
+            // Destination address information - IP and port
             InetAddress destinationIP = InetAddress.getByName("localhost");
             int destinationPort = 7777;
 
+            // LOGIC STAGE
             // Message to be sent
             String message = "Hello world!";
+
+            // TRANSMISSION STAGE:
             // Condition the message for transmission
             byte[] payload = message.getBytes();
             // Build the packet to be sent
@@ -27,11 +30,18 @@ public class SampleClient {
             // Send message to server
             mySocket.send(packet);
 
-
+            // Create a byte array to hold the payload data
             byte [] receivedMessage = new byte[50];
+            // Create a packet to hold the received message
             DatagramPacket incomingMessage = new DatagramPacket(receivedMessage, receivedMessage.length);
+            // Receive the message from the network
+            // This will BLOCK until it receives a message, i.e. the code will not progress beyond this line!
             mySocket.receive(incomingMessage);
+            // Get the data out of the packet
             receivedMessage = incomingMessage.getData();
+
+            // LOGIC STAGE
+            // Display the data from the packet
             System.out.println("Message received: " + receivedMessage.toString());
         } catch (UnknownHostException e) {
             System.out.println("IP address is not recognised");
