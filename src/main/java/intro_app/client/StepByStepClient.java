ad1646028b12244ef67a0e927696876e5198863e
michelle <michelle.graham@dkit.ie>
2024-01-31 21:33:29 +0000

    Added some extra explanatory commenting.

--- src/main/java/intro_app/client/StepByStepClient.java
+++ src/main/java/intro_app/client/StepByStepClient.java
@@ -9,24 +9,73 @@ public class StepByStepClient {
         Scanner input = new Scanner(System.in);
         // Establish address information - OUR network port
         int myPort = 10909;
-        // Create OUR window to the network - our socket for receiving messages
+
+        DatagramSocket mySocket = null;
         try {
-            DatagramSocket mySocket = new DatagramSocket(myPort);
+            // Create OUR window to the network - our socket for receiving messages
+            mySocket = new DatagramSocket(myPort);
+            /*
+                Logic overview:
+                    Repeatedly:
+                        Phase 1 (Sending):
+                            Ask for message destination information
+                            Ask for the message to be sent
+                            Create a byte array from the message
+                            Create a packet and fill it with:
+                                The byte array of the message data
+                                The length of the data
+                                The destination IP for the message
+                                The destination port for the message
+                            Send the packet via the socket
+                        Phase 2 (Receiving):
+                            Create an empty byte array of the largest feasible size
+                            Create a packet and fill it with the blank array (so that it has somewhere to put the
+                            incoming data)
+                            Receive whatever the next incoming message is into the blank packet
+                            Extract the payload (received message) from the packet
+                            Convert the payload from a byte array to a String using the String constructor (this will let
+                             us only use the exact size of data the packet says it was carrying)
+                            Display the message to the user
+             */
+            while(true) {
+                System.out.println("Do you want to send a message? (Yes to continue, any other key to exit): ");
+                String choice = input.nextLine();
+                if(!choice.equalsIgnoreCase("yes")){
+                    break;
+                }
+                System.out.println("Please enter the hostname: ");
+                String hostname = input.nextLine();
+                // Create address information for SERVER
+                InetAddress serverIP = InetAddress.getByName(hostname);
 
-            // Create address information for SERVER
-            InetAddress serverIP = InetAddress.getByName("localhost");
-            int serverPort = 20990;
+                System.out.println("Please enter the port: ");
+                int serverPort = input.nextInt();
+                input.nextLine();
 
-            System.out.println("Please enter the message to send: ");
-            String message = input.nextLine();
-            // CONVERT message into a byte array
-            byte [] data = message.getBytes();
+                System.out.println("Please enter the message to send: ");
+                String message = input.nextLine();
+                // CONVERT message into a byte array
+                byte[] data = message.getBytes();
 
-            // Build datagram packet for transmission
-            DatagramPacket packet = new DatagramPacket(data, data.length, serverIP, serverPort);
+                // Build datagram packet for transmission
+                DatagramPacket packet = new DatagramPacket(data, data.length, serverIP, serverPort);
 
-            // SEND the message to the network
-            mySocket.send(packet);
+                // SEND the message to the network
+                mySocket.send(packet);
+
+                // Array to store incoming message
+                byte[] payload = new byte[1024 * 64];
+                // Packet to store incoming message
+                DatagramPacket incomingPacket = new DatagramPacket(payload, payload.length);
+                // Try to receive the message
+                System.out.println("Waiting for packet....");
+                mySocket.receive(incomingPacket);
+                System.out.println("Packet received!");
+
+                int len = incomingPacket.getLength();
+                String incomingMessage = new String(payload, 0, len);
+                System.out.println("Message reads: " + incomingMessage);
+            }
         } catch (BindException e) {
             System.out.println("Port already in use. Please supply a different port next time!");
         }catch (SocketException e){
@@ -35,6 +84,13 @@ public class StepByStepClient {
             System.out.println("Server IP hostname does not exist. Please update architecture information.");
         } catch (IOException e) {
             System.out.println("Problem occurred when sending the message. Please try again later.");
+        }finally {
+            if(mySocket != null){
+                mySocket.close();
+            }
         }
+
+        System.out.println("Thank you for using our messaging system!");
+
     }
 }
--- src/main/java/intro_app/server/StepByStepServer.java
+++ src/main/java/intro_app/server/StepByStepServer.java
@@ -8,23 +8,27 @@ public class StepByStepServer {
     public static void main(String[] args) {
         // Establish server (MY) listening port
         int myPort = 20990;
+        DatagramSocket mySocket = null;
         
         // Create a window for server to access network
         try {
             
-            DatagramSocket mySocket = new DatagramSocket(myPort);
+            mySocket = new DatagramSocket(myPort);
 
             while(true) {
                 // Array to store incoming message
-                byte [] payload = new byte[50];
+                byte [] payload = new byte[1024*64];
                 // Packet to store incoming message
                 DatagramPacket incomingPacket = new DatagramPacket(payload, payload.length);
                 // Try to receive the message
                 System.out.println("Waiting for packet....");
                 mySocket.receive(incomingPacket);
                 System.out.println("Packet received!");
-                String incomingMessage = new String(incomingPacket.getData()).strip();
+                
+                int len = incomingPacket.getLength();
+                String incomingMessage = new String(payload, 0, len);
                 System.out.println("Message reads: " + incomingMessage);
+                
 
                 // Get the address information from the received packet
                 InetAddress clientIP = incomingPacket.getAddress();
@@ -44,6 +48,12 @@ public class StepByStepServer {
             System.out.println("An error occurred while setting up local socket.");
         } catch (IOException e) {
             System.out.println("Problem occurred when waiting to receive/receiving a message. Please try again later.");
+        }finally {
+            if(mySocket != null){
+                mySocket.close();
+            }
         }
+
+        System.out.println("Server shutting down...");
     }
 }
