ff29a9b40e42eec1a2e80d3d1bceaec749fda69d
michelle <michelle.graham@dkit.ie>
2024-03-05 13:41:51 +0000

    Converted server to using utility class holding protocol requests and responses as constants. Added feature to find the text at a specific line

---
+++ src/main/java/file_server_system/FileServerProtocol.java
@@ -0,0 +1,17 @@
+package file_server_system;
+
+public class FileServerProtocol {
+    public static final int PORT = 5784;
+
+    // REQUEST STRINGS
+    public static final String EXISTS = "EXISTS";
+    public static final String LENGTH = "LENGTH";
+    public static final String ACCESSED = "LAST_ACCESSED";
+    public static final String LINE = "LINE";
+
+    // RESPONSE STRINGS
+    public static final String NOT_FOUND_RESPONSE = "FILE_NOT_FOUND";
+    public static final String FOUND_RESPONSE = "FOUND";
+    public static final String INVALID = "INVALID_COMMAND";
+    public static final String BOUNDS = "OUT_OF_BOUNDS";
+}
--- src/main/java/file_server_system/server/FileServer.java
+++ src/main/java/file_server_system/server/FileServer.java
@@ -1,55 +1,143 @@
 package file_server_system.server;
 
+import file_server_system.FileServerProtocol;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.*;
-import java.time.LocalDate;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.TimeZone;
 
 public class FileServer {
     public static void main(String[] args) {
         // Establish server (MY) listening port
-        int myPort = 5784;
-        DatagramSocket mySocket = null;
-        
-        // Create a window for server to access network
-        try {
-            
-            mySocket = new DatagramSocket(myPort);
+        int myPort = FileServerProtocol.PORT;
 
-            while(true) {
+        // Create a window for server to access network
+        try (DatagramSocket mySocket = new DatagramSocket(myPort)) {
+
+            while (true) {
                 // Array to store incoming message
-                byte [] payload = new byte[1024*64];
+                byte[] payload = new byte[1024 * 64];
                 // Packet to store incoming message
                 DatagramPacket incomingPacket = new DatagramPacket(payload, payload.length);
                 // Try to receive the message
                 System.out.println("Waiting for packet....");
                 mySocket.receive(incomingPacket);
                 System.out.println("Packet received!");
-                
+
                 int len = incomingPacket.getLength();
                 String incomingMessage = new String(payload, 0, len);
                 System.out.println("Message reads: " + incomingMessage);
-                String [] components = incomingMessage.split("%%");
+                String[] components = incomingMessage.split("%%");
 
                 // NOW I know what the client has sent - incomingMessage
                 // Create a variable to hold the outgoing message
                 String outgoing = null;
-                switch(components[0]){
-                    case "EXISTS":
-                        // Do all logic for what should be done where the user wants to check if a file exists
-                        String filename = components[1];
-                        File userFile = new File(filename);
-                        if(userFile.exists()){
-                            outgoing = "FOUND";
-                        }else{
-                            outgoing = "FILE_NOT_FOUND";
+                String filename;
+                File f;
+                switch (components[0]) {
+                    case FileServerProtocol.EXISTS:
+                        if (components.length == 2) {
+                            // Do all logic for what should be done where the user wants to check if a file exists
+                            filename = components[1];
+                            f = new File(filename);
+                            if (f.exists()) {
+                                outgoing = FileServerProtocol.FOUND_RESPONSE;
+                            } else {
+                                outgoing = FileServerProtocol.NOT_FOUND_RESPONSE;
+                            }
+                        } else {
+                            outgoing = FileServerProtocol.INVALID;
+                        }
+                        break;
+                    case FileServerProtocol.ACCESSED:
+                        if (components.length == 2) {
+                            filename = components[1];
+                            f = new File(filename);
+
+                            if (f.exists()) {
+                                long lastModified = f.lastModified();
+                                LocalDateTime accessDate =
+                                        LocalDateTime.ofInstant(Instant.ofEpochMilli(lastModified),
+                                                TimeZone.getDefault().toZoneId());
+                                outgoing =
+                                        new String(accessDate.getDayOfWeek() + "" +
+                                                accessDate.atZone(TimeZone.getDefault().toZoneId()));
+                            } else {
+                                outgoing = FileServerProtocol.NOT_FOUND_RESPONSE;
+                            }
+                        } else {
+                            outgoing = FileServerProtocol.INVALID;
+                        }
+                        break;
+                    case FileServerProtocol.LINE:
+                        if (components.length == 3) {
+                            filename = components[1];
+                            f = new File(filename);
+
+                            if (f.exists()) {
+                                try {
+                                    // Get the line number at which to stop
+                                    int lineNum = Integer.parseInt(components[2]);
+                                    if(lineNum < 0){
+                                        throw new IllegalArgumentException("Line count cannot be less than 0");
+                                    }
+
+                                    // Get appropriate line from file
+                                    Scanner file = new Scanner(f);
+                                    String line = null;
+                                    int count = 0;
+                                    while (file.hasNext() && count != lineNum){
+                                        line = file.nextLine();
+                                        count++;
+                                    }
+
+                                    // Decide whether to send back line or error
+                                    if(count == lineNum){
+                                        outgoing = line;
+                                    }else{
+                                        outgoing = FileServerProtocol.BOUNDS;
+                                    }
+                                }catch(NumberFormatException e){
+                                    System.out.println("Line number supplied is not a number: " + components[2]);
+                                    outgoing = FileServerProtocol.INVALID;
+                                }catch(IllegalArgumentException e){
+                                    System.out.println("Line number supplied is an invalid value: " + components[2]);
+                                    System.out.println(e.getMessage());
+                                    outgoing = FileServerProtocol.BOUNDS;
+                                }
+                            }else {
+                                outgoing = FileServerProtocol.NOT_FOUND_RESPONSE;
+                            }
+                        } else {
+                            outgoing = FileServerProtocol.INVALID;
+                        }
+                        break;
+                    case FileServerProtocol.LENGTH:
+                        if (components.length == 2) {
+
+                            filename = components[1];
+                            f = new File(filename);
+
+                            if (f.exists()) {
+                                long size = f.length();
+                                outgoing = size + "";
+
+                            } else {
+                                outgoing = FileServerProtocol.NOT_FOUND_RESPONSE;
+                            }
+                        } else {
+                            outgoing = FileServerProtocol.INVALID;
                         }
                         break;
                     default:
                         // Set the outgoing message for all unrecognised commands
-                        outgoing = "INVALID_COMMAND";
+                        outgoing = FileServerProtocol.INVALID;
                 }
-                
+
 
                 // Get the address information from the received packet
                 InetAddress clientIP = incomingPacket.getAddress();
@@ -63,16 +151,12 @@ public class FileServer {
 
                 mySocket.send(sendingPacket);
             }
-        }catch(BindException e){
+        } catch (BindException e) {
             System.out.println("Port already in use. Please supply a different port next time!");
-        }catch (SocketException e){
+        } catch (SocketException e) {
             System.out.println("An error occurred while setting up local socket.");
         } catch (IOException e) {
             System.out.println("Problem occurred when waiting to receive/receiving a message. Please try again later.");
-        }finally {
-            if(mySocket != null){
-                mySocket.close();
-            }
         }
 
         System.out.println("Server shutting down...");
