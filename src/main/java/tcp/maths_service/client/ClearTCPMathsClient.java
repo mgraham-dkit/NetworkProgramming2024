6595a993db4e241ca84b0baff0516c431048cfc5
michelle <michelle.graham@dkit.ie>
2024-03-18 16:19:48 +0000

    Updated client to move from clear client to menu-driven, user-focused client.
    Includes code redesign (moving request generation to separate method, extracting code to safely take in a number into a separate method) and menu logic, plus request generation based on known protocol components.

--- src/main/java/tcp/maths_service/client/ClearTCPMathsClient.java
+++ src/main/java/tcp/maths_service/client/ClearTCPMathsClient.java
@@ -1,12 +1,12 @@
 package tcp.maths_service.client;
 
-import tcp.file_server_system.FileServerProtocol;
 import tcp.maths_service.server.MathsService;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ClearTCPMathsClient {
@@ -25,7 +25,7 @@ public class ClearTCPMathsClient {
                 while(validSession) {
                     // Ask user for information to be sent
                     System.out.println("Please enter a message to be sent (Send EXIT to end):");
-                    String message = userInput.nextLine();
+                    String message = generateRequest(userInput);
                     // Send message to server
                     output.println(message);
                     // Flush message through to server
@@ -48,5 +48,67 @@ public class ClearTCPMathsClient {
             System.out.println("An IO Exception occurred: " + e.getMessage());
         }
         // Close connection to server
+    }
+
+    public static void displayMenu(){
+        System.out.println("0) Exit");
+        System.out.println("1) Calculate square of a number");
+        System.out.println("2) Calculate cube of a number");
+        System.out.println("3) Show largest number I have calculated during this session");
+        System.out.println("4) Show largest number ever calculated in server's lifetime");
+    }
+
+    public static String generateRequest(Scanner userInput){
+        boolean valid = false;
+        String request = null;
+
+        while(!valid) {
+            displayMenu();
+            String choice = userInput.nextLine();
+            int value = 0;
+            switch (choice) {
+                case "0":
+                    System.out.println("Good choice?");
+                    request = MathsService.EXIT;
+                    break;
+                case "1":
+                    value = getValidInt(userInput, "Please enter the number to be squared: ");
+                    request = MathsService.SQUARE + MathsService.DELIMITER + value;
+                    break;
+                case "2":
+                    value = getValidInt(userInput, "Please enter the number to be cubed: ");
+                    request = MathsService.CUBE + MathsService.DELIMITER + value;
+                    break;
+                case "3":
+                    request = MathsService.MYLARGEST;
+                    break;
+                case "4":
+                    request = MathsService.LARGEST;
+                    break;
+                default:
+                    System.out.println("Please select one of the stated options!");
+                    System.out.println("------------------------------------");
+                    continue;
+            }
+            valid = true;
+        }
+        return request;
+    }
+
+    public static int getValidInt(Scanner userInput, String prompt) {
+        boolean valid = false;
+        int value = 0;
+        while(!valid) {
+            System.out.println(prompt);
+            try {
+                value = userInput.nextInt();
+                valid = true;
+            }catch(InputMismatchException e){
+                System.out.println("Numeric data required!");
+                userInput.nextLine();
+            }
+        }
+        userInput.nextLine();
+        return value;
     }
 }
